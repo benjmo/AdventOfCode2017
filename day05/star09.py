ff58554263f8310f1b0827442233ade6ab28d0a5
Benjamin Mo <b.mo96@hotmail.com>
2017-12-08 19:03:53 +1100

    day05 complete

---
+++ day05/star09.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import re
+
+def main():
+   with open("./input.txt", 'r') as file:
+      instructions = list(map(int, file.read().split('\n')))
+      pos = 0
+      i = 0
+      while (pos >= 0 and pos < len(instructions)):
+         oldPos = pos
+         pos += instructions[pos]
+         instructions[oldPos] += 1
+         i += 1
+      print(i)
+
+
+if __name__ == "__main__":
+    main()
