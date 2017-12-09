57b4957e4b0e78f45ed03204c71c4e6d8f71fb8f
Benjamin Mo <b.mo96@hotmail.com>
2017-12-09 16:02:54 +1100

    day06 complete

---
+++ day06/star11.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+import re
+
+def main():
+   with open("./input.txt", 'r') as file:
+      data = file.read()
+   memory = list(map(int, data.split()))
+   
+   visited = {}
+   memString = ' '.join(list(map(str, memory)))
+   visited[memString] = True
+
+   cycles = 0
+   while True:
+      maxPos = memory.index(max(memory))
+      maxMem = memory[maxPos]
+      memory[maxPos] = 0
+
+      currPos = (maxPos + 1) % len(memory)
+      for i in range(maxMem, 0, -1):
+         memory[currPos] += 1
+         currPos = (currPos + 1) % len(memory)
+      cycles += 1
+      
+      memString = ' '.join(list(map(str, memory)))
+      if (memString in visited):
+         break
+      else:
+         visited[memString] = True
+
+   print(cycles)
+
+
+if __name__ == "__main__":
+    main()
